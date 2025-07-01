01b1903280c24098d1b52b8ef86449542dca349c
Ata molkarae <molkaraeeata@gmail.com>
2025-07-02 00:48:26 +0300

    day-15-2

---
+++ src/app/login/page.tsx
@@ -0,0 +1,86 @@
+"use client";
+import { useState } from "react";
+import { supabase } from "../lib/supabaseClient";
+import ToastList from "../component/ui/ToastList";
+import { motion, AnimatePresence } from "framer-motion";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) {
+      alert("Login failed" + error.message);
+      {
+        /* <ToastList>{error.message}</ToastList>; */
+      }
+    } else {
+      alert("Logging In");
+    }
+  };
+  const handleSignUp = async () => {
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+    if (error) {
+      alert("Sign Up failed" + error.message);
+    } else {
+      alert("Signed Up");
+    }
+  };
+  const handleGoogle = async () => {
+    await supabase.auth.signInWithOAuth({ provider: "google" });
+  };
+
+  return (
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        className="p-10 max-w-md mx-auto w-[40%] space-y-4 flex flex-col items-center justify-center"
+      >
+        <h1 className="text-2xl font-bold"> Login / SignUp </h1>
+        <input
+          value={email}
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          className="outline-blue-600/70 w-full py-2 px-4"
+        />
+        <input
+          value={password}
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+          className="outline-blue-600/70 w-full py-2 px-4"
+        />
+        <div className="flex flex-col gap-2">
+          <motion.button
+            whileTap={{ scale: 0.97 }}
+            className="bg-black text-white px-4 py-2 rounded  w-full "
+            onClick={handleLogin}
+          >
+            Login
+          </motion.button>
+          <motion.button
+            whileTap={{ scale: 0.97 }}
+            className="bg-gray-700 text-white px-4 py-2 rounded w-full "
+            onClick={handleSignUp}
+          >
+            Sign Up
+          </motion.button>
+          <motion.button
+            whileTap={{ scale: 0.97 }}
+            className="bg-stone-200 text-white px-4 py-2 rounded hover:shadow shadow-stone-500 transition-all duration-75 w-full "
+            onClick={handleGoogle}
+          >
+            <img src="./google.svg" className="mx-auto w-8 h-8 " />
+          </motion.button>
+        </div>
+      </motion.div>
+    </AnimatePresence>
+  );
+}
